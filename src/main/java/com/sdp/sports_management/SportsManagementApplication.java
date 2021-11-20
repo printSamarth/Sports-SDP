a7a515bcfd5db19d01856e916accd9ece774fd96
printSamarth <itssp1998@gmail.com>
2021-11-20 11:00:59 +0530

    upadted

---
+++ src/main/java/com/sdp/sports_management/SportsManagementApplication.java
@@ -0,0 +1,13 @@
+package com.sdp.sports_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SportsManagementApplication.class, args);
+    }
+
+}
