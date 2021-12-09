0bec6360e67a9ccac55358240546dd0740d3e6de
nihar01 <nihar_kanwar@yahoo.co.in>
2021-12-09 11:33:27 +0530

    adding Tournament component

---
+++ src/main/frontend/src/components/TournamentComponent.js
@@ -0,0 +1,50 @@
+import React, {Component} from "react"
+import {Link} from "react-router-dom"
+
+
+class TournamentComponent extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            tournamentId: this.props.tournamentId,
+            sportName: this.props.sportName,
+            maxTeam: this.props.maxTeam,
+            noTeam: this.props.noTeam,
+            captainId: this.props.captainId //add captainId to team bean
+            
+        }   
+    }
+
+
+    render() {
+        return (
+            <div className = "card col-md-6 offset-md-3" style={{borderRadius:"25px",padding :"20px"}}>
+            <div className="card-body">
+            
+            <div className="TournamentComponent" href="" style={{width: '30rem'}}>
+                
+                <h3>{this.state.sportName}</h3>
+                <p>Tournament Id: {this.state.tournamentId}</p>
+                <p>Sport Name:{this.state.sportName}</p>
+                <p>Max Teams:{this.state.maxTeam}</p>
+                
+                <p>No. of Teams:{this.state.noTeam} </p>
+                <p>Captain Id:{this.state.captainId} </p>
+                
+                <Link to={{pathname:'/TeamList',tournamentId:this.state.tournamentId }}>
+                    <button className="btn btn-primary">Individual Participation</button>
+                </Link>
+
+                <Link to={{pathname:'/CreateTeam',tournamentId:this.state.tournamentId }}>
+                    <button className="btn btn-primary">Team Participation</button>
+                </Link>
+            </div>
+           
+            </div>
+            </div>
+
+        )
+    }
+}
+
+export default TournamentComponent;
