ba56bced7aed77514c8979839e9acecea37c0489
printSamarth <itssp1998@gmail.com>
2021-12-10 17:19:58 +0530

    Name sync with backend.

--- src/main/frontend/src/services/TournamentService.js
+++ src/main/frontend/src/services/TournamentService.js
@@ -1,17 +1,18 @@
 import axios from 'axios';
 
-const TOURNAMENT_API_BASE_URL=""
+const TOURNAMENT_API_BASE_URL="http://localhost:8081/api/tournament/save/"
+const TOURNAMENT_API_GET_URL = "http://localhost:8081/api/getAllTournaments/"
 class TournamentService{
 
     getTournament()
     {
-        return axios.get(TOURNAMENT_API_BASE_URL);   //get the data from the API mentioned
+        return axios.get(TOURNAMENT_API_GET_URL);   //get the data from the API mentioned
     }
 
     createTournament(tournament)
     {
         console.log('in login user service', tournament);
-        return axios.post(TOURNAMENT_API_BASE_URL, tournament);   //get the data from the API mentioned
+        return axios.put(TOURNAMENT_API_BASE_URL, tournament);   //get the data from the API mentioned
 
     }
 
