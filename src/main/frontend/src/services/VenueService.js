813de4e305be547ce5c335eed0d642ab2cc896f5
shwetamasrani <shwetamasrani19@gmail.com>
2021-12-11 12:11:42 +0530

    venue service

--- src/main/frontend/src/services/VenueService.js
+++ src/main/frontend/src/services/VenueService.js
@@ -5,9 +5,25 @@ class VenueService{
 
     getVenue()
     {
+        console.log("Caught in service");
         return axios.get(Venue_API_BASE_URL);   //get the data from the API mentioned
-       
     }
 
+    createVenue(venue)
+    {
+        return axios.post(Venue_API_BASE_URL,venue);
+    }
+
+    // getUserById(userId){
+    //     return axios.get(USER_API_BASE_URL + '/' + userId);
+    // }
+
+    // updateUser(user, userId){
+    //     return axios.put(USER_API_BASE_URL + '/' + userId, user);
+    // }
+
+    // deleteUser(userId){
+    //     return axios.delete(USER_API_BASE_URL + '/' + userId);
+    // }
 }
 export default new VenueService()   //exporting the object of this class
