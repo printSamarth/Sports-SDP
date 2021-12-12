5d7ea3a61e08dc8940d4225e74fe0f68c21d02a7
printSamarth <itssp1998@gmail.com>
2021-12-12 12:32:50 +0530

    Tournament details

--- src/main/frontend/src/components/TournamentComponent.js
+++ src/main/frontend/src/components/TournamentComponent.js
@@ -9,7 +9,9 @@ class TournamentComponent extends Component {
             tournamentId: this.props.tournamentId,
             sportName: this.props.sportName,
             maxTeam: this.props.maxTeam,
-            noTeam: this.props.noTeam
+            noTeam: this.props.noTeam,
+            venueName: this.props.venueName,
+            venueAddress:this.props.venueAddress
             
         }   
     }
@@ -24,10 +26,12 @@ class TournamentComponent extends Component {
                 
                 <h3>{this.state.sportName}</h3>
                 <p>Tournament Id: {this.state.tournamentId}</p>
-                <p>Sport Name:{this.state.sportName}</p>
-                <p>Max Teams:{this.state.maxTeam}</p>
+                <p>Sport Name: {this.state.sportName}</p>
+                <p>Max Teams: {this.state.maxTeam}</p>
                 
-                <p>No. of Teams:{this.state.noTeam} </p>
+                <p>No. of Teams: {this.state.noTeam} </p>
+                <p>Venue Name: {this.state.venueName} </p>
+                <p>Venue Address: {this.state.venueAddress} </p>
                 
                 <Link to={{pathname:'/TeamList',tournamentId:this.state.tournamentId }}>
                     <button className="btn btn-primary">Individual Participation</button>
