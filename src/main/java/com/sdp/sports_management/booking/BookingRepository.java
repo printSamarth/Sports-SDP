aec8aa9ba0a3a0586bdc25b9e426c9246acf3e1c
printSamarth <itssp1998@gmail.com>
2021-12-12 10:50:39 +0530

    Added BookingRepository.

---
+++ src/main/java/com/sdp/sports_management/booking/BookingRepository.java
@@ -0,0 +1,10 @@
+package com.sdp.sports_management.booking;
+
+import com.sdp.sports_management.bean.Booking;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookingRepository extends JpaRepository<Booking, Integer> {
+
+}
