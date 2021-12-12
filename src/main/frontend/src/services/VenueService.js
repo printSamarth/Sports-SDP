8a57605e515485eda9f97eba7ff630193fbcd769
printSamarth <itssp1998@gmail.com>
2021-12-12 10:54:37 +0530

    Change in venue service

--- src/main/frontend/src/services/VenueService.js
+++ src/main/frontend/src/services/VenueService.js
@@ -1,28 +1,19 @@
 import axios from 'axios';
 
-const Venue_API_BASE_URL="http://localhost:8081/api/venue"
+const AVAILABLE_VENUE_API_BASE_URL="http://localhost:8081/api/venue/availableVenues/"
+const Venue_API_BASE_URL = ""
 class VenueService {
 
-    getVenue() {
+    getVenue(date) {
 
-        console.log("Caught in service");
-        return axios.get(Venue_API_BASE_URL);   //get the data from the API mentioned
+        console.log("Caught in service", date);
+        return axios.put(AVAILABLE_VENUE_API_BASE_URL, date);   //get the data from the API mentioned
     }
 
     createVenue(venue) {
         return axios.post(Venue_API_BASE_URL, venue);
     }
 
-    // getUserById(userId){
-    //     return axios.get(USER_API_BASE_URL + '/' + userId);
-    // }
-
-    // updateUser(user, userId){
-    //     return axios.put(USER_API_BASE_URL + '/' + userId, user);
-    // }
-
-    // deleteUser(userId){
-    //     return axios.delete(USER_API_BASE_URL + '/' + userId);
-    // }
+    
 }
 export default new VenueService()   //exporting the object of this class
