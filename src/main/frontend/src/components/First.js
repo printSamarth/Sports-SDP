b172d68dede97677de32adf05b871cc435ff6180
shwetamasrani <shwetamasrani19@gmail.com>
2021-12-16 10:01:26 +0530

    .

--- src/main/frontend/src/components/First.js
+++ src/main/frontend/src/components/First.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
-
+import "react-bootstrap-carousel/dist/react-bootstrap-carousel.css";
+import BsCarousel from "./BsCarousel";
 class First extends Component {
     render() {
         return (
             <div>
                 <h1 style={{ textAlign : "center", marginTop:"15px" }}>
-                    Welcome to Sportitup.
-                    Login to proceed
+                    Welcome to SportItUp
                 </h1>
+                <br></br>
+                <div>
+                <BsCarousel />
+                </div>
             </div>
         );
     }
